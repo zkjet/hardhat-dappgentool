3312e63fd483cc398d604ba5688f7e489f3280ab
zkjet <zkjet@mailfence.com>
2023-03-14 16:49:46 -0600

    added ethers, etherscan, polygon and polygonscan support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { ethers } from "ethers";
+import "@nomiclabs/hardhat-etherscan";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
@@ -6,6 +8,7 @@ dotenv.config();
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const INFURA_API_SECRET = process.env.INFURA_API_SECRET;
 
@@ -15,6 +18,9 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
+  polygonscan: {
+    apiKey: POLYGONSCAN_API_KEY,
+  },
   networks: {
     sepolia: {
       url: "https://sepolia.infura.io/v3/" + INFURA_API_KEY,
@@ -22,6 +28,10 @@ const config: HardhatUserConfig = {
     },
     goerli: {
       url: "https://goerli.infura.io/v3/" + INFURA_API_KEY,
+      accounts: [PRIVATE_KEY],
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.infura.io/v3/" + INFURA_API_KEY,
       accounts: [PRIVATE_KEY],
     }
   },
